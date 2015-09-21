44e521f094a30158db0f7469d27b835e44030839
coin <v.budnev@2kom.ru>
2015-09-21 15:25:21 +0300

    added test sceleton

---
+++ test/TestSceleton.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class TestSceleton{
+    @Test
+    public void testSceletonAlwaysOk(){
+
+        assertEquals(0,0);
+    }
+}
