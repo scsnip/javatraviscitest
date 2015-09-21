318fc5d1aa075589fc3aaa80f3a2c72a95e19118
coin <v.budnev@2kom.ru>
2015-09-21 17:07:04 +0300

    added gradle build so it populates all the jars inside of artifact jar

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,12 @@
+import org.apache.log4j.Logger;
+import org.apache.log4j.BasicConfigurator;
+
 
 public class Main {
+    public static final Logger logger = Logger.getLogger("Main");
     public static void main(String[] args) {
+        BasicConfigurator.configure();
+        logger.info("test from logger");
         System.out.println("Hello!");
     }
 }
